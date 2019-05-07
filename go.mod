8fb1b2bdfd7b0feba1d7c65dc674877510e90c5c
Zack Scholl <zack.scholl@gmail.com>
2019-05-07 16:26:33 -0700

    simplify test

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/schollz/logger
 
 go 1.12
-
-require github.com/stretchr/testify v1.3.0
--- logger_test.go
+++ logger_test.go
@@ -3,16 +3,22 @@ package logger
 import (
 	"bytes"
 	"fmt"
+	"strings"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestLogging(t *testing.T) {
 	logger := New()
 	var buf bytes.Buffer
 	logger.SetOutput(&buf)
-	logger.Trace("hello")
-	fmt.Println(buf.String())
-	assert.Equal(t, "", buf.String())
+
+	funcs := []func(string, ...interface{}){logger.Trace, logger.Debug, logger.Info, logger.Warn, logger.Error}
+	for i, s := range []string{"trace", "debug", "info", "warn", "error"} {
+		funcs[i](s)
+		fmt.Print(buf.String())
+		if !strings.Contains(buf.String(), s) {
+			t.Error("bad " + s)
+		}
+		buf.Reset()
+	}
 }
